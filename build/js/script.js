5e52a5304696ce9a537100674b405d9c6a858301
halleygz <halleluya2013idone@gmail.com>
2024-05-04 01:29:01 +0300

    made the contact me section work with emailjs

--- build/js/script.js
+++ build/js/script.js
@@ -1,14 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerBtn = document.getElementById('hamburger-button');
-    const mobileMenu = document.getElementById('mobile-menu');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
 
-    const toggleMenu = () => {
-        mobileMenu.classList.toggle('hidden');
-        mobileMenu.classList.toggle('flex');
-        hamburgerBtn.classList.toggle('toggle-btn');
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
+
+  hamburgerBtn.addEventListener("click", toggleMenu);
+
+  mobileMenu.addEventListener("click", toggleMenu);
+});
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
     };
 
-    hamburgerBtn.addEventListener('click', toggleMenu);
+    const serviceID = "service_ikivhll";
+    const templateID = "template_zms5wvl";
 
-    mobileMenu.addEventListener('click', toggleMenu);
-});
+    emailjs.init("V7Eu9xodbdUGFl_pA")
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message was sent successfully");
+      })
+      .catch((err) => console.log(err));
+  });
+
+// Remember to call sendMail() when the form is submitted
