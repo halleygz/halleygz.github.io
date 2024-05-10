163b12d9923ef9463ff820ab2218bd567bbe5920
halleygz <halleluya2013idone@gmail.com>
2024-05-10 09:35:45 +0300

    clean up on script.js

--- build/js/script.js
+++ build/js/script.js
@@ -1,39 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburgerBtn = document.getElementById("hamburger-button");
-  const mobileMenu = document.getElementById("mobile-menu");
-
-  const toggleMenu = () => {
-    mobileMenu.classList.toggle("hidden");
-    mobileMenu.classList.toggle("flex");
-    hamburgerBtn.classList.toggle("toggle-btn");
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
   };
 
-  hamburgerBtn.addEventListener("click", toggleMenu);
+  const serviceID = "service_ikivhll";
+  const templateID = "template_zms5wvl";
 
-  mobileMenu.addEventListener("click", toggleMenu);
+  emailjs.init("V7Eu9xodbdUGFl_pA");
+  emailjs.send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message was sent successfully");
+    })
+    .catch((err) => console.log(err));
 });
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-
-    const serviceID = "service_ikivhll";
-    const templateID = "template_zms5wvl";
-
-    emailjs.init("V7Eu9xodbdUGFl_pA")
-    emailjs
-      .send(serviceID, templateID, params)
-      .then((res) => {
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message was sent successfully");
-      })
-      .catch((err) => console.log(err));
-  });
