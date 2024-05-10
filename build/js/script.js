8a174d1094d12fba8ba41aaabcde0e56509774da
halleygz <halleluya2013idone@gmail.com>
2024-05-10 09:40:18 +0300

    scriptJS change

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
+
+  hamburgerBtn.addEventListener("click", toggleMenu);
+
+  mobileMenu.addEventListener("click", toggleMenu);
+});
 document.getElementById("contact-form").addEventListener("submit", function (event) {
   event.preventDefault();
   let params = {
